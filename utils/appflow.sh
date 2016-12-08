2c68e778d1b50e8449230905f7ff30bb1cb96960
89luca89 <luca.dimaio1@gmail.com>
2016-12-08 16:24:39 +0100

    Setup initial Hosts during installation

--- utils/appflow.sh
+++ utils/appflow.sh
@@ -11,15 +11,25 @@ else
 fi
 
 echo "#############################";
+echo "Configuring default Hosts..."
+echo "#############################";
+echo '192.168.80.2 atlantis
+192.168.80.3 atlantis.centos
+192.168.90.2 testing
+192.168.90.3 testing.centos' | sudo tee -a /etc/hosts
+
+echo "#############################";
 echo "Configuring default Dirs..."
 echo "#############################";
 mkdir -p ~/Documents/webdev;
 cd ~/Documents/webdev;
+
 echo "#############################";
 echo "Cloning AppFlow..."
 echo "#############################";
 git clone git@github.com:ttssdev/appflow.git;
 ln -s ~/Documents/webdev/appflow ~/appflow;
+
 echo "#############################";
 echo "Preparing the environment..."
 echo "#############################";
