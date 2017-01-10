9ba7bc268672a9cc1c819deeff125f61ab62a996
89luca89 <luca.dimaio1@gmail.com>
2017-01-10 17:21:06 +0100

    Fix tmate hardcoded path

--- utils/support.sh
+++ utils/support.sh
@@ -2,8 +2,8 @@
 
 source ~/.appflow/config
 
-/usr/bin/tmate -S /tmp/tmate.sock new-session -d
-/usr/bin/tmate -S /tmp/tmate.sock wait tmate-ready
-SESSION=`/usr/bin/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'`
+tmate -S /tmp/tmate.sock new-session -d
+tmate -S /tmp/tmate.sock wait tmate-ready
+SESSION=`tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'`
 
 ssh atlantis "echo $SESSION | mail -s 'Support Request' $CFG_APPFLOW_SUPPORT_MAIL"
