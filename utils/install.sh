840a87c489ca7eccba05db1db572b0c741d4c3ff
89luca89 <luca.dimaio1@gmail.com>
2017-02-01 12:17:52 +0100

    More Work: Bash Autocomplete

--- utils/install.sh
+++ utils/install.sh
@@ -9,7 +9,7 @@ sudo cp $HOME/appflow/appflow /usr/local/bin/appflow
 sudo chmod +x /usr/local/bin/appflow
 if [ $DIST == 'Darwin' ]; then
 	sudo cp $HOME/appflow/utils/autocomplete /usr/local/etc/bash_completion.d/appflow
-	source /usr/local/etc/bash_completion.d
+	source /usr/local/etc/bash_completion
 else
 	sudo cp $HOME/appflow/utils/autocomplete /etc/bash_completion.d/appflow
 	source /usr/share/bash-completion/bash_completion
