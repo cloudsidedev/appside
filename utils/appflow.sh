c516671c2d0b616b58c744da7ef3d17a124ae3a2
Ivo Marino <ivo.marino@ttss.ch>
2017-02-02 12:25:50 +0100

    Install ansible on macOS without --HEAD, see: https://github.com/ttssdev/appflow/issues/41\#issuecomment-276926596.

--- utils/appflow.sh
+++ utils/appflow.sh
@@ -10,7 +10,7 @@ if [ $DIST == 'Darwin' ]; then
 		brew install bash-completion bash
 	fi
 	if ! brew ls --versions ansible | grep HEAD; then
-		brew unlink ansible ; brew unlink ansible20 ; brew reinstall ansible --HEAD;
+		brew unlink ansible ; brew unlink ansible20 ; brew reinstall ansible;
 	fi
 	brew install caskroom/cask/brew-cask
 	if ! brew cask ls --versions virtualbox; then
@@ -37,17 +37,17 @@ else
 	cat /etc/os-release | grep -i ubuntu
 	if (( $? == 0 )); then
 		sudo add-apt-repository universe
-	fi 
+	fi
 	if [[ $PKG == "dnf" ]]; then
-		sudo $PKG groupinstall -y "C Development Tools and Libraries" "Development Tools" "Container Management" 
-		sudo $PKG install -y git vagrant bash-completion python-pip python python3 python-devel 
+		sudo $PKG groupinstall -y "C Development Tools and Libraries" "Development Tools" "Container Management"
+		sudo $PKG install -y git vagrant bash-completion python-pip python python3 python-devel
 	else
 		sudo $PKG update
 		sudo $PKG install -y git vagrant bash-completion python-pip python python3 libffi-dev \
 							python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev
 	fi
 	sudo pip install git+git://github.com/ansible/ansible.git;
-	
+
 	if (($? == 1)); then
 		echo "Error! Check your dependencies! Without vagrant and virtualbox
 you cannot use AppFlow, But it can still be installed.
@@ -120,7 +120,7 @@ cd $HOME/Documents/webdev/appflow
 make local ask-sudo-pass=true;
 
 if [[ $DIST == *"Linux"* ]]; then
-	virtualbox --help > /dev/null	
+	virtualbox --help > /dev/null
 	if (( $? != 0 )); then
 		echo "#############################";
 		echo "DONE!"
@@ -128,8 +128,8 @@ if [[ $DIST == *"Linux"* ]]; then
 		echo "#############################";
 		cd $currdir;
 		exit 1;
-	fi  
-fi 
+	fi
+fi
 
 echo "Would you like to initialize the Atlantis VM now?"
 read  -n 1 -p "[y/N]:" vm
