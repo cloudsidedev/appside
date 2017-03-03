8246575269e8b73d7c47199afae1df90f181a5ac
Ivo Marino <ivo.marino@ttss.ch>
2017-03-03 19:33:50 +0100

    Added TestCommand.

---
+++ src/Appflow/Command/TestCommand.php
@@ -0,0 +1,15 @@
+namespace AppFlow\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends Command
+{
+        // ...
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+    }
+}
