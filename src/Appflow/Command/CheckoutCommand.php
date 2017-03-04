2a2650dfa34fb72bcc311ec91e27b81fc3204672
Ivo Marino <ivo.marino@ttss.ch>
2017-03-04 15:38:37 +0100

    Added InputDefinition.

--- src/Appflow/Command/CheckoutCommand.php
+++ src/Appflow/Command/CheckoutCommand.php
@@ -13,6 +13,8 @@
 namespace AppFlow\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -31,11 +33,17 @@ class CheckoutCommand extends Command
 <info>Pull the latest encrpyted configuration for a given tenant from a repository</info>
 EOT
             )
+            ->setDefinition(
+                new InputDefinition(array(
+                    new InputOption('tenant', 't', InputOption::VALUE_REQUIRED),
+                    new InputOption('env', 'e', InputOption::VALUE_REQUIRED)
+                ))
+            );
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-         $output->writeln('TODO');
+         $output->writeln('TENANT: ' . $input->getOption('tenant') . ' ENV: ' . $input->getOption('env') );
     }
 }
