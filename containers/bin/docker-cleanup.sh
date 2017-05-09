24ebd519bf69bea844a89029285f8b3200f8a5dd
89luca89 <luca.dimaio1@gmail.com>
2017-05-09 16:39:48 +0200

    Test

--- containers/bin/docker-cleanup.sh
+++ containers/bin/docker-cleanup.sh
@@ -7,15 +7,15 @@ then
     docker rm $(docker ps -a -q)
     docker rmi $(docker images -q)
     docker volume rm $(docker volume ls -qf dangling=true)
-
+    
 else
 
     # stop and rm all containers
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
-
+    
     # stop and rm exited containers
     # docker stop $(docker ps -q -f status=exited)
     # docker rm $(docker ps -q -f status=exited)
-
+    
 fi
