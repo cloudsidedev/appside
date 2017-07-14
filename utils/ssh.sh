a50d981d11ea9cb4c5dc21451231822692ce41a0
89luca89 <luca.dimaio1@gmail.com>
2017-07-14 20:58:33 +0200

    Remove unnecessary check

--- utils/ssh.sh
+++ utils/ssh.sh
@@ -15,23 +15,23 @@ if [ $status -eq 0 ]; then
 	exit 1;
 fi
 
-if [ "$(uname)" == "Darwin" ]; then
-	which -s assh
-	if [[ $? != 0 ]] ; then
-	  brew install assh
-	fi
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	# check if assh is installed:
-	if [ ! -f "~/go/bin/assh" ]; then
-		echo 'export GOPATH=$HOME/go
-				export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-				alias ssh="assh wrapper ssh"' >> ~/.appflow/ssh.source
-		source ~/.appflow/ssh.source
-		go get -u github.com/moul/advanced-ssh-config/cmd/assh
-	fi
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-	echo 'Cygwin not currently supported'
-fi
+# if [ "$(uname)" == "Darwin" ]; then
+# 	which -s assh
+# 	if [[ $? != 0 ]] ; then
+# 	  brew install assh
+# 	fi
+# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+# 	# check if assh is installed:
+# 	if [ ! -f "~/go/bin/assh" ]; then
+# 		echo 'export GOPATH=$HOME/go
+# 				export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+# 				alias ssh="assh wrapper ssh"' >> ~/.appflow/ssh.source
+# 		source ~/.appflow/ssh.source
+# 		go get -u github.com/moul/advanced-ssh-config/cmd/assh
+# 	fi
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+# 	echo 'Cygwin not currently supported'
+# fi
 
 if [[ ! -d $HOME"/.ssh/assh.d" ]]; then
 	# first run, backup personal files
