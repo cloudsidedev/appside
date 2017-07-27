20af2a2af6ecf1f5b5aba4cce182dbcb0a94867a
Ivo Marino <ivo.marino@ttss.ch>
2017-07-27 13:26:51 +0200

    No Latex.

--- docs/conf.py
+++ docs/conf.py
@@ -198,48 +198,48 @@ html_show_copyright = False
 htmlhelp_basename = 'AppFlowdoc'
 
 
-# -- Options for LaTeX output --------------------------------------------------
+# # -- Options for LaTeX output --------------------------------------------------
 
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('book', 'AppFlow.tex', 'AppFlow Documentation',
-   'The AppFlow Collective', 'manual'),
-]
+# # Grouping the document tree into LaTeX files. List of tuples
+# # (source start file, target name, title, author, documentclass [howto/manual]).
+# latex_documents = [
+#   ('book', 'AppFlow.tex', 'AppFlow Documentation',
+#    'The AppFlow Collective', 'manual'),
+# ]
 
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-latex_logo = '_static/logo.pdf'
+# # The name of an image file (relative to this directory) to place at the top of
+# # the title page.
+# latex_logo = '_static/logo.pdf'
 
-latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '10pt',
-    'figure_align': 'H',
-}
+# latex_elements = {
+#     'papersize': 'a4paper',
+#     'pointsize': '10pt',
+#     'figure_align': 'H',
+# }
 
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
+# # For "manual" documents, if this is true, then toplevel headings are parts,
+# # not chapters.
+# #latex_use_parts = False
 
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
+# # If true, show page references after internal links.
+# #latex_show_pagerefs = False
 
-# If true, show URL addresses after external links.
-latex_show_urls = 'footnote'
+# # If true, show URL addresses after external links.
+# latex_show_urls = 'footnote'
 
-# Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
+# # Additional stuff for the LaTeX preamble.
+# #latex_preamble = ''
 
-# Documents to append as an appendix to all manuals.
-latex_appendices = [
-    'support',
-    'resources',
-    'changes',
-    'authors',
-]
+# # Documents to append as an appendix to all manuals.
+# latex_appendices = [
+#     'support',
+#     'resources',
+#     'changes',
+#     'authors',
+# ]
 
-# If false, no module index is generated.
-#latex_domain_indices = True
+# # If false, no module index is generated.
+# #latex_domain_indices = True
 
 
 # -- Options for manual page output --------------------------------------------
