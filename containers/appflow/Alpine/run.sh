f0a6426cb58907dfe0abb1d4fb0616aa2be07128
Luca Di Maio <luca.dimaio1@gmail.com>
2017-07-28 18:27:23 +0200

    Some Cleanup

--- containers/appflow/Alpine/run.sh
+++ containers/appflow/Alpine/run.sh
@@ -8,20 +8,14 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 
 # NOTE: /home/appflow exists already due to volume mounts (just need to set perms)
-#useradd --shell /bin/bash -u $USER_ID -o -c "" -M appflow
 adduser -s "/bin/bash" -u $USER_ID -g "" -D -H appflow
 
 export HOME=/home/appflow
 
-# chown -R appflow:appflow /opt/appflow
 chown appflow:appflow /home/appflow
 mkdir -p /home/appflow/tmp/.ssh/cm
 chown -R appflow:appflow /home/appflow/tmp
-# chown -R appflow:appflow /home/appflow/.ansible
 echo 'alias ssh="assh wrapper ssh"' >> /home/appflow/.bashrc
-env >> /home/appflow/.bashrc
-# exec echo appflow "$@"
-# su - appflow -c "appflow $@"
 PARAMS="$@"
 
 
