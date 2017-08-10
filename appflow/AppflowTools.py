58a700162add48250c51d0cd8dbc5f29ba230bdf
89luca89 <luca.dimaio1@gmail.com>
2017-08-10 03:19:19 +0200

    Code Cleanup

--- appflow/AppflowTools.py
+++ appflow/AppflowTools.py
@@ -9,8 +9,7 @@ import difflib
 
 
 def gitReset(tenant, env):
-    dir = os.getenv("HOME") + "/.appflow/tenant/" + \
-        tenant + "/"
+    dir = utils.getTenantDir(tenant)
     PIPE = subprocess.PIPE
     process = subprocess.Popen(
         ['git', '-C', dir, 'clean -xdf', env], stdout=PIPE, stderr=PIPE)
@@ -19,42 +18,31 @@ def gitReset(tenant, env):
         ['git', '-C', dir, 'checkout', env], stdout=PIPE, stderr=PIPE)
     process.communicate()
 
-    md5StoreFile = os.getenv("HOME") + "/.appflow/tmp/.appflow-" + \
-        os.getenv("USER") + "/" + tenant + "/appflow-" + env + "-md5"
-    try:
-        os.remove(md5StoreFile)
-    except IOError:
-        pass
-    try:
-        os.remove(md5StoreFile + "-new")
-    except IOError:
-        pass
+    md5StoreFolder = utils.getMD5folder(tenant)
+    md5StoreFile = md5StoreFolder + "/appflow-" + env + "-md5"
+    utils.safeRemove(md5StoreFile)
+    utils.safeRemove(md5StoreFile + "-new")
 
 
 def gitStatus(tenant, env):
-    dir = os.getenv("HOME") + "/.appflow/tenant/" + \
-        tenant + "/"
+    dir = utils.getTenantDir(tenant)
     targetFolder = dir + env
     if(utils.checkStringInFile(targetFolder + "/inventory", 'AES256')):
-        print ('Files Already Encrypted')
+        print('Files Already Encrypted')
         PIPE = subprocess.PIPE
         process = subprocess.Popen(
             ['git', '-C', dir, 'diff-files --name-only -B -R -M', env], stdout=PIPE, stderr=PIPE)
         process.communicate()
         return False
     else:
-        md5StoreFolder = os.getenv(
-            "HOME") + "/.appflow/tmp/.appflow-" + os.getenv("USER") + "/" + tenant
+        md5StoreFolder = utils.getMD5folder(tenant)
         md5StoreFile = md5StoreFolder + "/appflow-" + env + "-md5"
         md5StoreFileNew = md5StoreFolder + "/appflow-" + env + "-md5-new"
-        try:
-            os.remove(md5StoreFileNew)
-        except IOError:
-            pass
+        utils.safeRemove(md5StoreFileNew)
         fileList = utils.getFileList(targetFolder)
         for file in fileList:
             utils.writeMD5sum(file, md5StoreFileNew)
-        
+
         diff = utils.diffFiles(md5StoreFile, md5StoreFileNew)
         print('Changed files:')
         print('\n'.join(diff))
