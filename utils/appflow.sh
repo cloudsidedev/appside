5679681d8951e4cb92d64b98525387ca0f71349a
89luca89 <luca.dimaio1@gmail.com>
2017-12-18 17:44:52 +0100

    Merge 89-appflow-python in Master

---
+++ utils/appflow.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+currdir=`pwd`
+
+cd $HOME
+echo "Installing dependencies...";
+echo "#############################";
+DIST=`uname -s`
+if [ $DIST == 'Darwin' ]; then
+	if ! brew ls --versions bash-completion | grep HEAD; then
+		brew install bash-completion bash
+	fi
+	if ! brew ls --versions ansible | grep HEAD; then
+		brew unlink ansible ; brew unlink ansible20 ; brew reinstall ansible;
+	fi
+	brew install caskroom/cask/brew-cask
+	if ! brew cask ls --versions virtualbox; then
+		brew cask install virtualbox
+	fi
+	if ! brew cask ls --versions vagrant; then
+		brew cask install vagrant
+	fi
+	if ! brew cask ls --versions vagrant-manager; then
+		brew cask install vagrant-manager
+	fi
+else
+	declare -A osInfo;
+	osInfo[/etc/redhat-release]=dnf
+	osInfo[/etc/SuSE-release]=zypp
+	osInfo[/etc/debian_version]=apt-get
+	PKG=
+	for f in ${!osInfo[@]}
+	do
+		if [[ -f $f ]]; then
+			PKG=${osInfo[$f]}
+		fi
+	done
+	cat /etc/os-release | grep -i ubuntu
+	if (( $? == 0 )); then
+		sudo add-apt-repository universe
+	fi
+	if [[ $PKG == "dnf" ]]; then
+		sudo $PKG groupinstall -y "C Development Tools and Libraries" "Development Tools" "Container Management"
+		sudo $PKG install -y git vagrant bash-completion python-pip python python3 python-devel
+	else
+		sudo $PKG update
+		sudo $PKG install -y git vagrant bash-completion python-pip python python3 libffi-dev \
+							python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev
+	fi
+	sudo pip install git+git://github.com/ansible/ansible.git;
+
+	if (($? == 1)); then
+		echo "Error! Check your dependencies! Without vagrant and virtualbox
+you cannot use AppFlow, But it can still be installed.
+Would You Like to continue?"
+
+		read  -n 1 -p "[y/N]:" vm
+		echo ""
+		case $vm in
+			[Yy]* ) ;;
+			[Nn]* ) exit 1;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	fi
+fi
+
+echo "#############################";
+echo "Configuring default Hosts..."
+echo "#############################";
+grep -q -F '192.168.80.2 atlantis' /etc/hosts || sudo tee -a /etc/hosts<<<'192.168.80.2 atlantis'
+grep -q -F '192.168.80.3 atlantis.centos' /etc/hosts || sudo tee -a /etc/hosts<<<'192.168.80.3 atlantis.centos'
+grep -q -F '192.168.90.2 testing' /etc/hosts || sudo tee -a /etc/hosts<<<'192.168.90.2 testing'
+grep -q -F '192.168.90.3 testing.centos' /etc/hosts || sudo tee -a /etc/hosts<<<'192.168.90.3 testing.centos'
+
+echo "#############################";
+echo "Configuring default Dirs..."
+echo "#############################";
+mkdir -p $HOME/Documents/webdev;
+cd $HOME/Documents/webdev;
+
+echo "#############################";
+echo "Cloning AppFlow..."
+echo "#############################";
+if [[ -d appflow ]]; then
+	cd appflow
+	git pull;
+	cd ..
+else
+	git clone https://github.com/ttssdev/appflow.git;
+fi
+ln -s $HOME/Documents/webdev/appflow $HOME/appflow;
+
+
+echo "#############################";
+echo "Installing AppFlow..."
+echo "#############################";
+sudo mkdir -p /usr/local/bin/
+sudo cp $HOME/appflow/appflow /usr/local/bin/appflow
+sudo chmod +x /usr/local/bin/appflow
+if [ $DIST == 'Darwin' ]; then
+	sudo cp $HOME/appflow/utils/autocomplete /usr/local/etc/bash_completion.d/appflow
+	source /usr/local/etc/bash_completion
+else
+	sudo cp $HOME/appflow/utils/autocomplete /etc/bash_completion.d/appflow
+	source /usr/share/bash-completion/bash_completion
+fi
+
+echo "#############################";
+echo "Preparing the environment..."
+echo "#############################";
+
+mkdir -p $HOME/.appflow/tenant
+# ln -s $HOME/Documents/webdev/appflow/examples/appflow-mrrobot $HOME/.appflow/tenant/appflow-mrrobot
+# ln -s $HOME/.appflow/tenant/appflow-mrrobot $HOME/.appflow/tenant/mrrobot
+# ln -s $HOME/Documents/webdev/appflow/examples/vault $HOME/.appflow/tenant/vault
+mkdir -p $HOME/.appflow/tenant/appflow-mrrobot/
+rsync -av $HOME/Documents/webdev/appflow/examples/appflow-mrrobot/ $HOME/.appflow/tenant/appflow-mrrobot/
+# ln -s $HOME/.appflow/tenant/appflow-mrrobot $HOME/.appflow/tenant/mrrobot
+cp $HOME/Documents/webdev/appflow/config.example $HOME/.appflow/config
+cd $HOME/Documents/webdev/appflow
+make local ask-sudo-pass=true;
+
+if [[ $DIST == *"Linux"* ]]; then
+	virtualbox --help > /dev/null
+	if (( $? != 0 )); then
+		echo "#############################";
+		echo "DONE!"
+		echo "To utilize atlantis and vagrant, please install VIRTUALBOX from repos"
+		echo "#############################";
+		cd $currdir;
+		exit 1;
+	fi
+fi
+
+echo "Would you like to initialize the Atlantis VM now?"
+read  -n 1 -p "[y/N]:" vm
+echo ""
+case $vm in
+	[Yy]* ) echo "Choose your distro:
+1- Ubuntu 14.04
+2- CentOS/RHEL 7.1 (Experimental, still not complete!) "; read  -n 1 -p "[choose a number]:" dist;;
+	[Nn]* ) ;;
+	* ) echo "Please answer yes or no.";;
+esac
+
+echo ""
+
+if [ ! -z $dist ]; then
+	if [ $dist = "1" ]; then
+		DIS=
+	elif [ $dist = "2" ]; then
+		DIS="-centos"
+	else
+		echo "#############################";
+		echo "DONE!"
+		echo "#############################";
+
+		cd $currdir;
+		exit 0;
+	fi
+fi
+
+case $vm in
+	[Yy]* ) make vagrant$DIS && vagrant up atlantis$DIS && appflow provision firstrun=true limit=atlantis$DIS;;
+	[Nn]* ) ;;
+	* ) echo "Please answer yes or no.";;
+esac
+
+echo ""
+
+
+echo "#############################";
+echo "DONE!"
+echo "#############################";
+
+cd $currdir;
---
+++ utils/ssh.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+tenant=$1
+vault=$2
+env=$3
+args=$4
+make_tenant=$5
+
+make decrypt tenant=$make_tenant env=$env
+
+status=`grep AES256 ~/.appflow/tenant/$tenant/$env/inventory > /dev/null; echo $?`
+if [ $status -eq 0 ]; then
+	echo "Files are encrpyted, first run \"make decrypt\"."
+	false;
+	exit 1;
+fi
+
+# if [ "$(uname)" == "Darwin" ]; then
+# 	which -s assh
+# 	if [[ $? != 0 ]] ; then
+# 	  brew install assh
+# 	fi
+# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+# 	# check if assh is installed:
+# 	if [ ! -f "~/go/bin/assh" ]; then
+# 		echo 'export GOPATH=$HOME/go
+# 				export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+# 				alias ssh="assh wrapper ssh"' >> ~/.appflow/ssh.source
+# 		source ~/.appflow/ssh.source
+# 		go get -u github.com/moul/advanced-ssh-config/cmd/assh
+# 	fi
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+# 	echo 'Cygwin not currently supported'
+# fi
+
+if [[ ! -d $HOME"/.ssh/assh.d" ]]; then
+	# first run, backup personal files
+	cp ~/.ssh/config ~/.ssh/config_personal
+fi
+
+mkdir -p ~/tmp/.ssh/cm
+mkdir -p ~/.ssh/assh.d/$vault
+cp -f ~/.appflow/tenant/$vault/$env/assh.yml ~/.ssh/assh.d/$vault/$env.yml
+#assh config build > ~/.ssh/config
+make reset tenant=$make_tenant env=$env
+# Restore original config hosts
+#cat ~/.ssh/config_personal >> ~/.ssh/config
+assh info | grep $vault
