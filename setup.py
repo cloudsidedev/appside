c24eb4750a2f849091e9f9cedf108a68081e1955
89luca89 <luca.dimaio1@gmail.com>
2018-01-09 12:10:06 +0100

    WIP: pip packaging, pick the right license

--- setup.py
+++ setup.py
@@ -88,7 +88,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: AGPLv3 License',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
